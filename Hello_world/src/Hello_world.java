e8bc83bd34f56a7811f500a827f86a97b033788f
yibao1 <31105406+yibao1@users.noreply.github.com>
2017-08-20 21:22:26 -0700

    adding eclipse workspace and Hello_world program

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,13 @@
+/**
+ * My Hello_world program.
+ * 
+ * @author Emi Okuyama
+ * @version 2017-08-20
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From emi"); //Display the string
+    }
+}
