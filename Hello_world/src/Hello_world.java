7d8b4736c92367822ad0d466f1147829ceb478bc
yibao1 <31105406+yibao1@users.noreply.github.com>
2017-08-20 21:33:00 -0700

    testing

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -8,6 +8,6 @@ public class Hello_world
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From emi"); //Display the string
+        System.out.println("Hello World! From Emi."); //Display the string
     }
 }
