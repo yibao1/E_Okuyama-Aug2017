e3eed9dab3001681c9420027dcc3154647df295f
Emi Okuyama <18okuyamae55@millshighsch.org>
2017-08-22 09:28:34 -0700

    This is emi okuyama second project -- using git from eclipse

---
+++ Project2/src/Project2.java
@@ -0,0 +1,13 @@
+ 
+//Emi Okuyama
+//My second java project. It will compare print and printin.
+//August 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+         System.out.println("Hello -- this is project 2!!");
+         
+	}
+
+}
