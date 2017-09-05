4d588effd78ed8985fde33abf33e2375857eef8d
Emi Okuyama <18okuyamae55@millshighsch.org>
2017-09-05 09:26:18 -0700

    This is a calculate Library

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -24,31 +24,31 @@ public class Calculate {
 	 * This method takes two doubles and returns the average.
 	 */
 	public static double average(double operand1, double operand2) {
-		double average=(operand1 + operand2)/2;
+		double average = (operand1 + operand2) / 2;
 		return average;
 	}
 
 	/*
 	 * This method takes 3 doubles and returns the average.
 	 */
-	public static double average(double operand1, double operand2,double operand3) {
-		double average=(operand1 + operand2 + operand3)/3;
+	public static double average(double operand1, double operand2, double operand3) {
+		double average = (operand1 + operand2 + operand3) / 3;
 		return average;
 	}
-    /*
-     * This method converts an angle measure given in radians into degrees.
-     */
-     public static double toDegrees(double operand) {
-    	double toDegrees=(operand * (180/3.14159));
-    	return toDegrees;
-     }
-/*
- * toRadians
- */
-     public static double toRadians(double operand) {
-    	 double toRadians=(operand * (3.14159/180));
-    	 return toRadians;
-     }
-}
 
-	
+	/*
+	 * This method converts an angle measure given in radians into degrees.
+	 */
+	public static double toDegrees(double operand) {
+		double toDegrees = (operand * (180 / 3.14159));
+		return toDegrees;
+	}
+
+	/*
+	 * toRadians
+	 */
+	public static double toRadians(double operand) {
+		double toRadians = (operand * (3.14159 / 180));
+		return toRadians;
+	}
+}
