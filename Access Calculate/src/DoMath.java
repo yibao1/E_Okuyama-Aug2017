38842dc5d244f6fc468a076b186d13b51aaee677
Emi Okuyama <18okuyamae55@millshighsch.org>
2017-09-05 09:27:07 -0700

    This is the Access Calculate

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,17 @@
+/* Emi Okuyama
+ * September 5th, 2017
+ * This is the runner class for my Calculate Library methods.
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(2));
+		System.out.println(Calculate.cube(2));
+		System.out.println(Calculate.average(2, 6));
+		System.out.println(Calculate.average(3,3,3));
+		System.out.println(Calculate.toDegrees(3.14159));
+		System.out.println(Calculate.toRadians(180));
+
+	}
+
+}
