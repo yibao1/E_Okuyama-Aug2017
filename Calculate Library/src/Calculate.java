762b526963c89afcc5dcf4b2fa52a8472908dd89
Emi Okuyama <18okuyamae55@millshighsch.org>
2017-09-05 08:55:49 -0700

    I made a pikachu.

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,12 @@
+/* Emi Okuyama
+ * September 5th, 2017
+ * This is our home grown library that will contain various mathematical methods. 
+ */
+public class Calculate {
+
+/* This method takes an integer and returns its square.
+ */
+	public static int square(int operand) {
+		return operand * operand;
+	}
+}
---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,14 @@
+//Emi Okuyama
+//August 28 2017
+//picture of pikachu
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.println("\t Pikachu welcomes you to the world of pokemon");
+		System.out.println("\t \t \t \t \t (\\__/)");
+		System.out.println("\t \t \t \t \t (o^.^)");
+		System.out.println("         \t \t \t \t z(_(\")(\"))");
+
+	}
+
+}
