d00cd54dc981a66ffb9833098cb86ee808609908
Emi Okuyama <18okuyamae55@millshighsch.org>
2017-10-03 09:24:41 -0700

    partner project started

---
+++ Collaborative Programming Exercise/src/CollaborativeProgrammingExercise.java
@@ -0,0 +1,20 @@
+
+/*
+ * Emi Okuyama
+ * October 3rd, 2017
+ * Create a program that processes numbers
+ */
+
+import java.util.*;
+
+public class CollaborativeProgrammingExercise {
+	Scanner console = new Scanner(System.in);
+	String n = console.nextLine();
+	public static void main(String[] args) {
+		int sum = 0;
+		int min = 0;
+		int max = 0;
+		// obtain values
+
+	}
+}
