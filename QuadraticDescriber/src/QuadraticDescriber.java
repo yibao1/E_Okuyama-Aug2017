8245fc5906b65f02b9ac9a74e363967e0b293ecc
Emi Okuyama <18okuyamae55@millshighsch.org>
2017-10-10 08:47:49 -0700

    started part 5

---
+++ QuadraticDescriber/src/QuadraticDescriber.java
@@ -0,0 +1,21 @@
+
+/*
+ * Emi Okuyama
+ * October 10th, 2017
+ * This is a program that can describe the graph of a quadratic function.
+ */
+import java.util.*;
+
+public class QuadraticDescriber {
+
+	public static String quadrDescriber(double a, double b, double c) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Provide values for a, b, and c");
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		double c = scanner.nextDouble();
+				
+
+	}
+
+}
