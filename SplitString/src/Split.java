648fc0bbd1ab5683d5f53ef7a6b7ab42381ecdc2
Emi Okuyama <emi.yibao@gmail.com>
2017-10-26 21:33:16 -0700

    getn that bred

--- SplitString/src/Split.java
+++ SplitString/src/Split.java
@@ -1,40 +1,65 @@
+import java.util.Arrays;
 
 public class Split {
+	/*
+	 * Write a method that take in a string like
+	 * "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a
+	 * sandwich use String.split to split up the sandwich by the word "bread" and
+	 * return what's in the middle of the sandwich and ignores what's on the outside
+	 * What if it's a fancy sandwich with multiple pieces of bread?
+	 */
+	public static String[] splitSandwich(String ingredients) {
+		String[] parts = ingredients.split("bread");
+		if (parts.length <= 1) {
+			String[] result = {};
+			return result;
+		}
+		return Arrays.copyOfRange(parts, 1, parts.length - 1);
+
+	}
+
+	/*
+	 * Write a method that take in a string like
+	 * "apples pineapples bread lettus tomato bacon mayo ham bread cheese"
+	 * describing a sandwich use String.split to split up the sandwich at the
+	 * spaces, " ", and return what's in the middle of the sandwich and ignores
+	 * what's on the outside Again, what if it's a fancy sandwich with multiple
+	 * pieces of bread?
+	 */
+	public static String splitSandwichWithSpaces(String ingredients) {
+		String[] parts = ingredients.split(" ");
+		String result = "";
+		int firstBread = -1;
+		int lastBread = -1;
+		for (int i = 0; i < parts.length; i++) {
+			if (parts[i].equals("bread")) {
+				if (firstBread == -1) {
+					firstBread = i;
+				} else {
+					lastBread = i;
+				}
+			}
+		}
+		for (int j = firstBread + 1; j < lastBread; j++) {
+			if (!parts[j].equals("bread")) {
+				result += parts[j] + " ";
+			}
+		}
+		return result;
+	}
 
 	public static void main(String[] args) {
+		System.out.println("task1:");
+		System.out.println(Arrays.toString(splitSandwich("airbreadcheesebreadmeatbreadplate")));
+		System.out.println(Arrays.toString(splitSandwich("applespineapplesbreadlettustomatobaconmayohambreadcheese")));
+		System.out.println(Arrays.toString(splitSandwich("")));
+		System.out.println(Arrays.toString(splitSandwich("airbreadplate")));
+		System.out.println("task2:");
+		System.out
+				.println(splitSandwichWithSpaces("apples pineapples bread lettus tomato bacon mayo ham bread cheese"));
+		System.out.println(
+				splitSandwichWithSpaces("apples pineapples bread lettus tomato bread bacon mayo ham bread cheese"));
 		
-		// String.split();
-		// It's a method that acts on a string, <StringName>.split(<String sp>);
-		// Where sp is the string where the string splits
-		// And it returns an array
-		// Example: "I like apples!".split(" ");
-		// it will split at spaces and return an array of ["I","like","apples!"]
-		// Example 2: "I really like really red apples".split("really")
-		// it will split at the word "really" and return an array of ["I "," like ","
-		// red apples!"]
-
-		// play around with String.split! what happens if you "I reallyreally like
-		// apples".split("really") ?
-
-		// Your task:
-		/*
-		 * Write a method that take in a string like
-		 * "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a
-		 * sandwich use String.split to split up the sandwich by the word "bread" and
-		 * return what's in the middle of the sandwich and ignores what's on the outside
-		 * What if it's a fancy sandwich with multiple pieces of bread?
-		 */
-
-		// Your task pt 2:
-		/*
-		 * Write a method that take in a string like
-		 * "apples pineapples bread lettus tomato bacon mayo ham bread cheese"
-		 * describing a sandwich use String.split to split up the sandwich at the
-		 * spaces, " ", and return what's in the middle of the sandwich and ignores
-		 * what's on the outside Again, what if it's a fancy sandwich with multiple
-		 * pieces of bread?
-		 */
-
 	}
 
 }
