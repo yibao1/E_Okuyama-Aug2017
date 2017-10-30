c6e3de7ee6995120f01e2c69edb080baba7f27fd
Emi Okuyama <18okuyamae55@millshighsch.org>
2017-10-30 08:31:41 -0700

    made some comments

--- SplitString/src/Split.java
+++ SplitString/src/Split.java
@@ -1,3 +1,4 @@
+
 /*
  * Emi Okuyama
  * October 26th, 2017
@@ -14,24 +15,27 @@ public class Split {
 	 * What if it's a fancy sandwich with multiple pieces of bread?
 	 */
 	public static String[] splitSandwich(String ingredients) {
-		if (ingredients.indexOf("bread") == ingredients.lastIndexOf("bread")) { 
+		// if there is only one bread return an empty array
+		if (ingredients.indexOf("bread") == ingredients.lastIndexOf("bread")) {
 			String[] result = {};
 			return result;
 		}
+		// split at bread
 		String[] parts = ingredients.split("bread");
-		System.out.println("###" + ingredients + ":" + Arrays.toString(parts));
+		// if there are no ingredients in the sandwich then return an empty array
 		if (parts.length <= 1) {
 			String[] result = {};
 			return result;
 		}
 
-// if the string contains ingredients after the final bread then we need to remove them.  
+		// if the string contains ingredients after the final bread then we need to
+		// remove them.
 		int endOfParts;
 		if (ingredients.endsWith("bread")) {
 			endOfParts = parts.length;
 		} else {
 			endOfParts = parts.length - 1;
-		}			
+		}
 		return Arrays.copyOfRange(parts, 1, endOfParts);
 	}
 
@@ -44,6 +48,7 @@ public class Split {
 	 * pieces of bread?
 	 */
 	public static String splitSandwichWithSpaces(String ingredients) {
+		// split at " "
 		String[] parts = ingredients.split(" ");
 		String result = "";
 		int firstBread = -1;
@@ -75,18 +80,14 @@ public class Split {
 		System.out.println(Arrays.toString(splitSandwich("breadhambread")));
 		System.out.println(Arrays.toString(splitSandwich("breadbread")));
 		System.out.println("task2:");
-		System.out.println(
-				splitSandwichWithSpaces("bread ham bread"));
+		System.out.println(splitSandwichWithSpaces("bread ham bread"));
 		System.out
 				.println(splitSandwichWithSpaces("apples pineapples bread lettus tomato bacon mayo ham bread cheese"));
 		System.out.println(
 				splitSandwichWithSpaces("apples pineapples bread lettus tomato bread bacon mayo ham bread cheese"));
-		System.out.println(
-				splitSandwichWithSpaces("bread bread"));
-		System.out.println(
-				splitSandwichWithSpaces(""));
-		System.out.println(
-				splitSandwichWithSpaces("bread ham"));
+		System.out.println(splitSandwichWithSpaces("bread bread"));
+		System.out.println(splitSandwichWithSpaces(""));
+		System.out.println(splitSandwichWithSpaces("bread ham"));
 	}
 
 }
