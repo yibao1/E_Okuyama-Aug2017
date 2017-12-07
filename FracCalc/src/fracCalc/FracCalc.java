4ae432a4920689a69ca011ec7d2a7683294e83cf
Emi Okuyama <emi.yibao@gmail.com>
2017-12-06 22:55:10 -0800

    FINISHED FRAC CALC

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,3 +1,6 @@
+//Emi Okuyama
+//November 21, 2017
+
 package fracCalc;
 
 import java.util.*;
@@ -39,64 +42,22 @@ public class FracCalc {
 		int[] op2Arr = splitString(op2);
 		int whole1 = op1Arr[0], num1 = op1Arr[1], denom1 = op1Arr[2];
 		int whole2 = op2Arr[0], num2 = op2Arr[1], denom2 = op2Arr[2];
-		int whole = 0;
-		int denom = 1;
-		int num = 0;
-		if (oper.equals("-")) {
-			oper = "+";
-			if (whole2 == 0) {
-				num2 *= -1;
-			} else {
-				whole2 *= -1;
-			}
-		} else if (oper.equals("/")) {
-			if (whole2 < 0) {
-				num2 *= -1;
-			}
-			oper = "*";
-			num2 = toImproperFracNumerator(whole2, num2, denom2);
-			whole2 = 0;
-			int temp = denom2;
-			denom2 = num2;
-			num2 = temp;
-		}
-		if (whole1 < 0) {
-			num1 *= -1;
-		}
-		if (whole2 < 0) {
-			num2 *= -1;
-		}
-		
-		System.out.println(oper + " " + whole1 + " " + num1 + " " + denom1);
-		System.out.println(oper + " " + whole2 + " " + num2 + " " + denom2);
-		if (oper.equals("+")) {
-			whole = whole1 + whole2;
-			denom = gcd(denom1, denom2);
-			num = 0;
-			if (denom1 != denom) {
-				num += num1 * (denom / denom1);
-			} else {
-				num += num1;
-			}
-			if (denom2 != denom) {
-				num += num2 * (denom / denom2);
-			} else {
-				num += num2;
-			}
-		} else if (oper.equals("*")) {
-			denom = denom1 * denom2;
-			num = toImproperFracNumerator(whole1, num1, denom1) * toImproperFracNumerator(whole2, num2, denom2);
-			whole = num / denom;
-			num = num % denom;
-		}
+		int[] result = handleOperation(oper, whole1, num1, denom1, whole2, num2, denom2);
+		int whole = result[0], num = result[1], denom = result[2];
 
-		if (whole < 0 && num < 0) {
-			num *= -1;
+		// simplify mixed number
+		whole += num / denom;
+		num = num % denom;
+		int factor = gcf(num, denom);
+		num /= factor;
+		denom /= factor;
+
+		// if there is no fractional part just return the whole part
+		if (num == 0) {
+			return "" + whole;
 		}
-		// return oper + " " + whole1 + " " + num1 + " " + denom1;
 		return whole + "_" + num + "/" + denom;
 	}
-
 
 	// TODO: Implement this function to produce the solution to the input
 	public static int[] splitString(String input) {
@@ -120,6 +81,72 @@ public class FracCalc {
 		return fraction;
 	}
 
+	public static int[] handleOperation(String oper, int whole1, int num1, int denom1, int whole2, int num2,
+			int denom2) {
+		int whole = 0;
+		int denom = 1;
+		int num = 0;
+
+		// rewrite - as + of negatives and / as * of reciprocal
+		if (oper.equals("-")) {
+			oper = "+";
+			if (whole2 == 0) {
+				num2 *= -1;
+			} else {
+				whole2 *= -1;
+			}
+		} else if (oper.equals("/")) {
+			if (whole2 < 0) {
+				num2 *= -1;
+			}
+			oper = "*";
+			num2 = toImproperFracNumerator(whole2, num2, denom2);
+			whole2 = 0;
+			int temp = denom2;
+			denom2 = num2;
+			num2 = temp;
+		}
+
+		// when a whole number is negative transfer the sign to the numerator
+		if (whole1 < 0) {
+			num1 *= -1;
+		}
+		if (whole2 < 0) {
+			num2 *= -1;
+		}
+
+		// we only impliment two operations + and *
+		if (oper.equals("+")) {
+			whole = whole1 + whole2;
+			denom = lcd(denom1, denom2);
+			num = 0;
+			if (denom1 != denom) {
+				num += num1 * (denom / denom1);
+			} else {
+				num += num1;
+			}
+			if (denom2 != denom) {
+				num += num2 * (denom / denom2);
+			} else {
+				num += num2;
+			}
+		} else if (oper.equals("*")) {
+			denom = denom1 * denom2;
+			num = toImproperFracNumerator(whole1, num1, denom1) * toImproperFracNumerator(whole2, num2, denom2);
+			whole = num / denom;
+			num = num % denom;
+		}
+
+		// when both the whole part and numerator part are negative we only need to
+		// return the whole as negative
+		if (whole < 0 && num < 0) {
+			num *= -1;
+		}
+
+		int[] result = { whole, num, denom };
+		return result;
+	}
+
 	public static int toImproperFracNumerator(int number, int numerator, int denominator) {
 		int mixedNumerator = (number * denominator + numerator);
 		return mixedNumerator;
@@ -140,15 +167,23 @@ public class FracCalc {
 		return operand1 % operand2 == 0;
 	}
 
-	public static int gcd(int x, int y) {
+	public static int lcd(int x, int y) {
 		int result = x * y;
 		for (int i = 2; i <= min(x, y); i++) {
 			if (isDivisibleBy(x, i) && isDivisibleBy(y, i)) {
 				result = i;
 			}
 		}
-
 		return result;
+	}
 
+	public static int gcf(int x, int y) {
+		int result = 1;
+		for (int i = 1; i <= min(x, y); i++) {
+			if (isDivisibleBy(x, i) && isDivisibleBy(y, i)) {
+				result = i;
+			}
+		}
+		return result;
 	}
 }
