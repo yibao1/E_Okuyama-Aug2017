c0f7691fa742ceaab17a55ae51da71d70cef1aa5
Emi Okuyama <18okuyamae55@millshighsch.org>
2018-03-13 09:09:23 -0700

    cont checkpooint 2

--- TextExcel/src/textExcel/TextCell.java
+++ TextExcel/src/textExcel/TextCell.java
@@ -1,5 +1,19 @@
 package textExcel;
 
-public class TextCell {
+public class TextCell implements Cell{
+	
+	private String text;
+	
+	public String fullCellText(){
+		return "\"" + text + "\"";
+	}
 
+	@Override
+	public String abbreviatedCellText() {
+		String newText = text;
+		while (newText.length() < 10) {
+			newText += " ";
+		}
+		return newText;
+	}
 }
