28512fbfc414fa508061394d9ea4450e003f9d4c
Emi Okuyama <18okuyamae55@millshighsch.org>
2018-03-22 08:49:54 -0700

    oh my god

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -13,36 +13,40 @@ public class Spreadsheet implements Grid {
 	
 	public String processCommand(String command)
 	{
-		String[] parsed = command.split(" ", 3);
-		if(parsed.length == 1) {
-			if (Character.isAlphabetic(Character.toLowerCase(command.charAt(0))) && Character.isDigit(command.charAt(1))) {
-				Location thisCell = new SpreadsheetLocation(command);
-				return getCell(thisCell).fullCellText();
-			} else if (command.toLowerCase().equals("clear")) {
-				reset();
-				return getGridText();
-			}
-		} else if (parsed.length == 2) {
-			if (command.toLowerCase().startsWith("clear")) {
-				Location thisCell = new SpreadsheetLocation(parsed[1]);
-				excel[thisCell.getCol()][thisCell.getRow()] = new EmptyCell(); // check this out
-				return getGridText();
-			}
-		} else {
-			if (Character.isAlphabetic(Character.toLowerCase(command.charAt(0))) && Character.isDigit(command.charAt(1))) {
-				if (parsed[1].equals("=")) {
-					if (parsed[2].startsWith("" + '"')) {
-						Location thisCell = new SpreadsheetLocation(parsed[0].toLowerCase());
-						String input = parsed[2].substring(1, (parsed[2].length() - 1));
-						excel[thisCell.getCol()][thisCell.getRow()] = new TextCell(input); //check this out
-						return getGridText();
-					}
-				}
-			}
-		}
-		return "TEST";
-		}
-
+		int row, column;
+ 		String returnString = "";
+ 		if (command.contains("=")){
+ 			String[] data = command.split(" ", 3);
+ 			SpreadsheetLocation location = new SpreadsheetLocation(data[0]);
+ 			if (isNumeric(data[2])) {
+ 								ValueCell valueCell = new ValueCell(data[2]);
+ 								excel[location.getRow() + 1][location.getCol() + 1] = valueCell;	
+ 								return getGridText();
+ 							}
+ 							if (data[2].contains("%")) {
+ 								PercentCell percentCell = new PercentCell(data[2]);
+ 								excel[location.getRow() + 1][location.getCol() + 1] = percentCell;	
+ 								return getGridText();
+ 							}
+ 							if(!data[2].substring(0,1).equals("\"") && (command.contains("+") || command.contains("-") || command.contains("*") || command.contains("/"))) {
+ 								FormulaCell formulaCell = new FormulaCell(data[2]);
+ 								excel[location.getRow() + 1][location.getCol() + 1] = formulaCell;
+ 								return getGridText();
+ 							}
+ 							String [] contentsWithoutQuotes = data[2].split("\"", 3);
+ 							TextCell text = new TextCell(contentsWithoutQuotes[1]);
+ 							excel[location.getRow()+1][location.getCol()+1]= text;
+ 				 			return getGridText();
+ 				 		}
+ 				 		if (command.length() <= 3 && command.length() != 0) {
+ 				 			SpreadsheetLocation location = new SpreadsheetLocation(command);
+ 				 			return excel[location.getRow()+1][location.getCol()+1].fullCellText();
+ 				 		}
+ 				 		if (command.toLowerCase().contains("clear ")) {
+ 				 						String [] splitInput2 = command.split(" ", 2);
+ 				 						SpreadsheetLocation location = new SpreadsheetLocation(splitInput2[1]);
+ 				 						excel[location.getRow() + 1][location.getCol() + 1] = new EmptyCell();
+ 				 		}
 	public int getRows() {
 		return 20;
 	}
