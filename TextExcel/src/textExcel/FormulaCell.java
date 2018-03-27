e81f9aa1942ce09f35aa8e294ca9d766ea77c515
Emi Okuyama <18okuyamae55@millshighsch.org>
2018-03-27 09:15:49 -0700

    yikes on formula cell

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -1,9 +1,18 @@
 package textExcel;
 
 public class FormulaCell extends RealCell{
-
+	
 	public FormulaCell(String text) {
 		super(text);
 	}
 
+	//contains all the parsing and calculation logic required to generate a numeric result for an arithmetic formula 
+	//that does not contain cell references.
+	public double getDoubleValue() {
+		String[] parts = getText().split(" ");
+		double num;
+		if(parts[1].equals("*")) {
+			num = Double.parseDouble(parts[0]) * Double.parseDouble(parts[2]);
+		}
+	}
 }
