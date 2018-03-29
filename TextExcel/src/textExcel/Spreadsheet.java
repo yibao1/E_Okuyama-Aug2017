40b9c1c38aa9d2b4d9c21a8c9677a96c8e852607
Emi Okuyama <emi.yibao@gmail.com>
2018-03-28 21:22:33 -0700

    fixed checkpoint 1 and made variables to clean up spreadsheet.java

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -12,33 +12,37 @@ public class Spreadsheet implements Grid {
 
 	public String processCommand(String command) {
 		String lCommand = command.toLowerCase();
-		
-		
-		if (lCommand.equals("clear")) {
-			//clearing the entire sheet (e.g., clear).
+
+		if (lCommand.equals("")) {
+			//do nothing for empty command
+			return "";
+		} else if (lCommand.equals("clear")) {
+			// clearing the entire sheet (e.g., clear).
 			clear();
 		} else if (lCommand.startsWith("clear")) {
-			//clearing a particular cell (e.g., clear A1).
+			// clearing a particular cell (e.g., clear A1).
 			SpreadsheetLocation loc = new SpreadsheetLocation(command.split(" ")[1]);
 			cells[loc.getRow()][loc.getCol()] = new EmptyCell();
 		} else if (command.contains("=")) {
-			//assignment of string values (e.g., A1 = "Hello").
+			// assignment of string values (e.g., A1 = "Hello").
 			String[] parts = command.split(" ", 3);
+			String rhs = parts[2];
 			SpreadsheetLocation loc = new SpreadsheetLocation(parts[0]);
-			if (parts[2].endsWith("%")) {
-				//Percent assignment (e.g. A1 = 5.2%)
-				cells[loc.getRow()][loc.getCol()] = new PercentCell(parts[2].substring(0, parts[2].length() - 1));
-			} else if (parts[2].endsWith(")")){
-				cells[loc.getRow()][loc.getCol()] = new FormulaCell(parts[2]);
-			} else if (parts[2].endsWith("\"")){
-				cells[loc.getRow()][loc.getCol()] = new TextCell(parts[2].substring(1, parts[2].length() - 1));
+			int r = loc.getRow();
+			int c = loc.getCol();
+			if (rhs.endsWith("%")) {
+				// Percent assignment (e.g. A1 = 5.2%)
+				cells[r][c] = new PercentCell(rhs.substring(0, rhs.length() - 1));
+			} else if (rhs.endsWith(")")) {
+				cells[r][c] = new FormulaCell(rhs);
+			} else if (rhs.endsWith("\"")) {
+				cells[r][c] = new TextCell(rhs.substring(1, rhs.length() - 1));
 			} else {
-				//Real value assignment (e.g. A1 = 5.2)
-				cells[loc.getRow()][loc.getCol()] = new ValueCell(parts[2]);
+				// Real value assignment (e.g. A1 = 5.2)
+				cells[r][c] = new ValueCell(rhs);
 			}
-		
-		//cell inspection (e.g., A1)
 		} else {
+			// cell inspection (e.g., A1)
 			SpreadsheetLocation loc = new SpreadsheetLocation(command);
 			return cells[loc.getRow()][loc.getCol()].fullCellText();
 		}
@@ -57,31 +61,31 @@ public class Spreadsheet implements Grid {
 		return this.cells[loc.getRow()][loc.getCol()];
 	}
 
-	//Return a single String containing the entire spreadsheet grid.
+	// Return a single String containing the entire spreadsheet grid.
 	public String getGridText() {
 		String output = "   |";
 		String padding;
-		
-		//Makes the columns 
+
+		// Makes the columns
 		for (int i = 0; i < getCols(); i++) {
 			output += (char) (i + (int) 'A') + "         |";
 		}
 		output += "\n";
-		
-		//makes the rows
+
+		// makes the rows
 		for (int i = 0; i < getRows(); i++) {
 			if (i < 9) {
 				padding = "  ";
-			
-			//since double digits take more space use less spaces
+
+				// since double digits take more space use less spaces
 			} else {
 				padding = " ";
 			}
-			
-			//adds spaces
+
+			// adds spaces
 			output += (i + 1) + padding + "|";
-			
-			//puts stuff in each cell
+
+			// puts stuff in each cell
 			for (int j = 0; j < getCols(); j++) {
 				output += cells[i][j].abbreviatedCellText() + "|";
 			}
@@ -90,7 +94,7 @@ public class Spreadsheet implements Grid {
 		return output;
 	}
 
-	//clears spreadsheet
+	// clears spreadsheet
 	private void clear() {
 		for (int i = 0; i < getRows(); i++) {
 			for (int j = 0; j < getCols(); j++) {
